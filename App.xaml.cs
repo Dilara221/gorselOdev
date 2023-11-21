87e91377410c09db35620c09291f3fcb5ddf0b1c
Dilara221 <91744497+Dilara221@users.noreply.github.com>
2023-11-21 18:47:14 +0300

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace görselÖdevDeneme;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace görselÖdevDeneme;
+
+public partial class MainPage : ContentPage
+{
+	
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	
+}
+
+
